619f59be05de2129118ff51cf8021daa0a1a4d53
Frankiestiner <Alacornbob@gmail.com>
2019-05-02 10:34:21 -0400

    Initial commit

---
+++ app/src/main/java/com/fwhite/calendar_reminder_app/MainActivity.java
@@ -0,0 +1,24 @@
+package com.fwhite.calendar_reminder_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String APP_ID;
+        String API_KEYS;
+        APP_ID = getString(R.string.APP_ID);
+        API_KEYS = getString(R.string.API_KEYS);
+
+
+
+
+
+        
+    }
+}
